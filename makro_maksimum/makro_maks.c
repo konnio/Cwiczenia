752606be1d1e5648dd05f7a091cac17d0a7afd98
konnio <konnio@gmail.com>
2018-01-11 10:56:20 +0100

    makro maksimum

---
+++ makro_maksimum/makro_maks.c
@@ -0,0 +1,17 @@
+// Podstawy programowania - Cwiczenia
+// 11.01.2018
+// Cwiczenie 1 - Makro obliczajace maksimum
+
+#include<stdio.h>
+// a > b ? a : b
+#define MAKS(a,b) (((a)>(b))?(a):(b))
+#define MIN(a,b) (((a)>(b))?(a):(b))
+
+
+int main ()
+{
+	const int liczba1 = 1000;
+	const int liczba2 = 2;
+	printf("%d\n",MAKS(liczba1, liczba2));
+	return 0;
+}
